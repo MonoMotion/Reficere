e54ee164ca804eabc225bb69838c8a049e2be9e6
coord.e <me@coord-e.com>
2019-05-16 05:05:27 +0000

    Add: Use GPL v3

--- ReficereSystems.ino
+++ ReficereSystems.ino
@@ -1,8 +1,22 @@
-/*************************************
-  Reficere system for YamaX 7.0 or any robot
-  made by Y-modify, Nyanyan & coord.e
-  Copyright © 2016-2019 Y-modify All Rights Reserved.
-*************************************/
+//
+// Reficere system for YamaX 7.0 or any robot
+// Copyright © 2016-2019 Nyanyan All Rights Reserved.
+//
+// This file is part of Reficere.
+//
+// Reficere is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Reficere is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Reficere.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 //definition about weight sensors
 #define numOfSensor 4
